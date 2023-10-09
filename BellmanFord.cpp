01f71f7db953bcecfdfef254da233e5bb6d3e04f
Anurag De <anuragde2004@gmail.com>
2023-10-09 21:06:34 +0530

    Add files via upload

---
+++ BellmanFord.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+vector<int> bellman_ford(int V, vector<vector<int>> & edge, int S){
+    vector<int> dist(V, 1e8);
+    for(int i=0;i<V;i++){
+        for(auto it:edges){
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+
+            if(dist[u]!= 1e8 && dist[u + wt <dist[v]]) dist[v]=dist[u]+wt;
+            return dist;
+        }
+    }
+}
+int main(){
+
+}
